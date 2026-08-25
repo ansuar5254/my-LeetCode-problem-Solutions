ab22d78c5efbcd48e219e573b77e9c49f68f0a7a
ansuar5254 <ansuar5254@gmail.com>
2026-08-25 21:10:36 +0300

    Time: 0 ms (100%), Space: 19.2 MB (85.64%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.py
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.py
@@ -1,8 +1,5 @@
 class Solution:
     def minCost(self, n: int) -> int:
-        ans = 0
-        for i in range(1,n):
-            ans +=(n-i)
-
-        return ans
+        return n*(n-1)//2
+        
         
